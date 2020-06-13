6e02b37fd2f98af8f22ae16b02e10a1e54d028a9
zhouzengfa <275699969@qq.com>
2020-06-13 11:58:43 +0800

    topk算法

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+#include "topk/topk.h"
+
+int main(int argc, char *argv[])
+{
+	testing::InitGoogleTest(&argc, argv);//将命令行参数传递给gtest
+	return RUN_ALL_TESTS();   //RUN_ALL_TESTS()运行所有测试案例
+}
