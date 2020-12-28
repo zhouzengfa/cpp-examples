c0592ffb7137bf39c3cce6ee5d4d68ba38e3623a
zhouzengfa <275699969@qq.com>
2020-12-28 21:11:57 +0800

    log分模块

--- module_log/log_controller.cpp
+++ module_log/log_controller.cpp
@@ -1,28 +1,5 @@
-﻿#include "topk.h"
+﻿#include "log_controller.h"
 #include <gtest/gtest.h>
-
-class A
-{
-public:
-	A(int n) { a = n; }
-	~A() {};
-	int a = 0;
-
-	void print()
-	{
-		std::cout << a << ",";
-	}
-};
-
-/*由于STL自带优先队列是默认最大优先的，所以自己写了一个比较函数，将其改为最小优先*/
-class MaxComp
-{
-public:
-	bool operator()(A* a, A* b)
-	{
-		return a->a > b->a;		//最小值优先
-	}
-};
 
 TEST(TopK, testMaxTopK)
 {
@@ -48,37 +25,57 @@ TEST(TopK, testMaxTopK)
 	ASSERT_EQ(6,list[2]->a);
 }
 
-class MinComp
+LogModule::LogModule()
 {
-public:
-	bool operator()(A* a, A* b)
+	m_modules.clear();
+	m_bitData = new char[m_length];
+	memset(m_bitData, 0, m_length);
+}
+
+LogModule::~LogModule()
+{
+	delete m_bitData;
+}
+
+
+LogModule& LogModule::getInst()
+{
+	static LogModule inst;
+	return inst;
+}
+
+int LogModule::RegisterModule(std::string& moduleName)
+{
+	for (int i= 0; i < m_modules.size(); ++i)
 	{
-		return a->a < b->a;		//最小值优先
+		if (moduleName == m_modules[i])
+			return i;
 	}
-};
 
-TEST(TopK, testMinTopK)
+	int index = m_modules.size();
+	m_modules.push_back(moduleName);
+
+	if (index < m_length)
+	{
+		m_bitData[index] = 1;
+	}
+	else
+	{
+		char* p = new char[m_length * 2];
+		memcpy(p, m_bitData, m_length);
+		delete m_bitData;
+		m_bitData = p;
+		m_length *= 2;
+	}
+
+	return index;
+}
+
+bool LogModule::needLog(int moduleIndex)
 {
-	TopK<A*, MinComp> topk(3);
+	if (moduleIndex >= m_length)
+		return false;
 
-	A* p = new A(2);
-	topk.push(p);
-	p = new A(3);
-	topk.push(p);
-	p = new A(1);
-	topk.push(p);
-	p = new A(4);
-	topk.push(p);
-	p = new A(6);
-	topk.push(p);
-	p = new A(5);
-	topk.push(p);
-	auto list = topk.getTopK();
-	std::cout << std::endl;
-
-	ASSERT_EQ(3,list.size());
-	ASSERT_EQ(3,list[0]->a);
-	ASSERT_EQ(2,list[1]->a);
-	ASSERT_EQ(1,list[2]->a);
+	return m_bitData[moduleIndex] > 0;
 }
 
--- module_log/log_controller.h
+++ module_log/log_controller.h
@@ -1,57 +1,40 @@
 ﻿#pragma once
 
-/*
-*代码采用STL中的最小优先队列实现，由于STL中自带最小优先队列，其底层就是二叉堆实现，
-*所以就不再手写二叉堆了。最小优先队列顶层元素总是队列中最小的元素，也就是二叉堆堆顶。
-*/
-
 #include <iostream>
-#include <queue>
 #include <vector>
 using namespace std;
 
-template<class ObjType, class Comparator>
-class TopK
+class LogModule
 {
 public:
-	TopK(int k)
-	{
-		m_k = k;
-	}
+	LogModule();
+	~LogModule();
 
-	bool push(ObjType& obj)
-	{
-		if (minHeap.size() < m_k)
-		{
-			minHeap.push(obj);
-			return true;
-		}
-
-		if (Comparator()(obj, minHeap.top()))
-		{
-			minHeap.pop();
-			minHeap.push(obj);
-			return true;
-		}
-
-		return false;
-	}
-
-	std::vector<ObjType> getTopK()
-	{
-		std::vector<ObjType> list;
-		while (!minHeap.empty())
-		{
-			list.push_back(minHeap.top());
-			minHeap.top()->print();
-			minHeap.pop();
-		}
-
-		return std::move(list);
-	}
+	static LogModule& getInst();
+	int RegisterModule(std::string& moduleName);
+	bool needLog(int moduleIndex);
 
 private:
-	int m_k = 0;
-	priority_queue<ObjType, vector<ObjType>, Comparator> minHeap;			//建立最小优先队列
+	std::vector<std::string> m_modules;
+	char* m_bitData = nullptr;
+	int m_length = 1024;
 };
+
+#define DEFINE_LOG(module)	\
+class logDefine##module \
+{ \
+public: \
+	logDefine##module() \
+	{ \
+		index = LogModule::getInst().RegisterModule(##module); \
+	} \
+	bool needLog() \
+	{ \
+		return LogModule::getInst().needLog(index); \
+	} \
+private: \
+	int index = -1; \
+}; \
+logDefine##module a##module; \
+#define DEBUG !a##module.needLog() ? void(0) : std::cout << "["<<##module <<"]"
 
